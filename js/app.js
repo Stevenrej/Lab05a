086b78896f933a7dea359ce44d85d445b583e3ff
Steven Rejdukowski <steveo732@gmail.com>
2022-09-16 18:27:46 -0500

    solved prob 4

--- js/app.js
+++ js/app.js
@@ -78,15 +78,17 @@ IMPORTANT DETAIL: You may not use the arithmetic operator + in this function. To
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSumArray() function and see if the test passes.*/
 
 // Write your code here
-let testArray = [2, 3, 4]; //eslint-disable-line
+let testArray = [2, 3, 4];
 
-function sumArray(sumArr) { //eslint-disable-line
-
+function sumArray(sumArr) {
+  let sum4 = sum(sum(testArray[0],testArray[1])[0],testArray[2])[0];
+  let stringarr = `${testArray[0]},${testArray[1]},${testArray[2]} was passed in as an array of numbers, and ${sum4} is their sum.`;
+  return [sum4, stringarr];
 }
 
 // Here is the test for sumArray(); uncomment it to run it
 
-// testSumArray(testArray);
+testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -101,8 +103,10 @@ IMPORTANT DETAIL: You may not use the arithmetic operator * in this function. To
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiplyArray() function and see if the test passes.*/
 
 // Write your code here
-function multiplyArray(multArr) { //eslint-disable-line
-
+function multiplyArray(multArr) { 
+  let mult4 = multiply(multiply(testArray[0],testArray[1])[0],testArray[2])[0];
+  let stringmultarray = `The numbers ${testArray[0]},${testArray[1]},${testArray[2]} have a product of ${mult4}.`;
+  return [mult4, stringmultarray]
 }
 
 // Here is the test for multiplyArray(); uncomment it to run it
@@ -129,7 +133,7 @@ Test this function by hand in the console to get it working, and when you think 
 // Write your code here
 let testDynamicArray = [1,2,3,4,5]; //eslint-disable-line
 
-function multiplyAnyArray(dynamicArray) { //eslint-disable-line
+function multiplyAnyArray(dynamicArray) { 
 
 }
 
